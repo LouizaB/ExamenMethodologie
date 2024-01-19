5ab81595eb9c012a84f1854ce3b484cfc694e26c
LouizaB <146197939+LouizaB@users.noreply.github.com>
2024-01-19 14:13:43 +0100

    Last commit

--- projet.py
+++ projet.py
@@ -1,12 +1,12 @@
-# Dans le fichier projet.py
+
 class Projet:
     def __init__(self, nom, gestion_taches):
         self.nom = nom
         self.gestion_taches = gestion_taches
         self.titres_taches = []
 
-    def ajouterTacheAuProjet(self, titre, description):
-        self.gestion_taches.ajouterTache(titre, description)
+    def ajouterTacheAuProjet(self, titre, description,duree):
+        self.gestion_taches.ajouterTache(titre, description,duree)
         self.titres_taches.append(titre)
 
     def verifierTacheDansProjet(self, titre):
